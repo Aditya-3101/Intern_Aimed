4036dbb05fafbaf8f34c3995614354556eb1b10f
Aditya Dhayfule <adityadhayfule3101@gmail.com>
2023-10-28 23:11:54 +0530

    intial commit

--- src/App.js
+++ src/App.js
@@ -81,6 +81,10 @@ const InputField = styled.div`
   display: flex;
   flex-direction: column;
 
+  @media screen and (max-width: 768px) {
+    width: 95%;
+  }
+
   & p {
     font-family: "Poppins", sans-serif;
     font-weight: 500;
